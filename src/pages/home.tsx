a213879da0b6af408db4f47b8c348da43b4a4681
Amit Dubey <iamsaagar@yahoo.com>
2024-06-16 10:59:39 +0100

    updated

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useContext, useEffect, useRef, useState } from "react";
 import PageLayout from "../lib/components/page.layout";
 import { AppContext } from "../lib/context/app.context";
