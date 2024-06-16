dac4e10f16098fe084dab39b610d53b16c63751c
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-06-16 12:51:10 +0530

    update api routes

--- src/lib/constants/api.constants.ts
+++ src/lib/constants/api.constants.ts
@@ -15,15 +15,16 @@ export const API_BASE_URL = "https://certbe.onrender.com";
 export enum API_ROUTES {
   LOGIN = "/auth/login",
   REGISTER = "/auth/register",
-  APPROVE_USER = "/users/approve",
-  APPROVE_PFI = "/pfis/approve",
-  APPROVE_AGREEMENT = "/agreements/approve",
-  REJECT_AGREEMENT = "/agreements/reject",
-  GET_USER_BY_EMAIL = "/users/email",
-  GET_USER_BY_PFI = "/users/pfi",
+  APPROVE_USER = "/users/approve",  // do a /{user_id} to approve a specific user
+  APPROVE_PFI = "/pfis/approve",  // do a /{pfi_id} to approve a specific pfi
+  APPROVE_AGREEMENT = "/agreements/approve", // do a /{agreement_id} to approve a specific agreement
+  REJECT_AGREEMENT = "/agreements/reject", // do a /{agreement_id} to reject a specific agreement
+  GET_USER_BY_EMAIL = "/users/user",  // do a /{email} to get a specific user
+  GET_USER_BY_PFI = "/users/pfi", // do a /{parent_id} to get all users for a specific pfi
   GET_ALL_PFI = "/pfis",
-  GET_AGREEMENTS_BY_PFI = "/agreements/pfi",
-  CREATE_AGREEMENT = "/agreements/create",
-  GET_DOC_BY_AGREEMENT = "/documents/get-docs",
-  UPLOAD_DOC = "/documents/upload-file",
+  GET_AGREEMENTS_BY_PFI = "/agreements/pfi", // do a /{pfi_id} to get all agreements for a specific pfi
+  CREATE_AGREEMENT = "/agreements/create",  
+  GET_PENDING_REQUESTS = "/users/pending",
+  GET_DOC_BY_AGREEMENT = "/documents/get-docs", // do a /{agreement_id} to get all docs for an agreement
+  UPLOAD_DOC = "/documents/upload-file", // do a /{agreement_id} to upload a docs for an agreement
 }
